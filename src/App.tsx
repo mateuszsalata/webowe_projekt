a6e45556693d880fed16466b4974700a505cbbb8
Adrian Piechowiak <apiechowiak6572@gmail.com>
2024-05-30 16:22:36 +0200

    cos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import Example from './components/Switcher.tsx'
 import IconSideNav from './components/Navbar.tsx'
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
